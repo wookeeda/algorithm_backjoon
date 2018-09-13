3e5987f82bca419a39657464bea8d107cd17e8ed
wookeeda <wookeeda@gmail.com>
2018-09-13 22:30:59 +0900

    dp start

---
+++ src/Main1520.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main1520 {
+
+
+    private static int[][] board;
+    private static int[][] dp;
+    private static int m;
+    private static int n;
+    private static int[] x;
+    private static int[] y;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        board = new int[m][n];
+        dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        x = new int[]{1, 0, 0, -1};
+        y = new int[]{0, 1, -1, 0};
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+//        for (int i = 0; i < m; i++) { for (int j = 0; j < n; j++) System.out.print(board[i][j] + " ");System.out.println(); }
+
+        dp[m - 1][n - 1] = 1;
+        int cnt = climb(0, 0);
+
+        bw.write(String.valueOf(cnt));
+        bw.close();
+        br.close();
+    }
+
+    private static int climb(int a, int b) {
+        if (dp[a][b] > -1) {
+            return dp[a][b];
+        }
+        int cnt = 0;
+        for (int i = 0; i < 4; ++i) {
+            int a2 = a + x[i];
+            int b2 = b + y[i];
+
+            if (a2 >= 0 && a2 < m && b2 >= 0 && b2 < n && board[a][b] > board[a2][b2]) {
+                cnt += climb(a2, b2);
+            }
+        }
+        return dp[a][b] = cnt;
+    }
+}
--- src/Main6591.java
+++ src/Main6591.java
@@ -1,3 +1,48 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main6591 {
 
+    static int n;
+    static int k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while( true ){
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            if("0 0".equals(str)) break;
+
+             n = Integer.parseInt(st.nextToken());
+             k = Integer.parseInt(st.nextToken());
+
+            int [][] dp = new int[n+1][k+1];
+
+            for(int i = 1; i <= n ; ++i){
+                for (int j = 0; j <= Math.min(i, k); j++) {
+                    if( j == 0 || j == i){
+                        dp[i][j] = 1;
+                    }else {
+                        dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    }
+                }
+            }
+//            for (int i = 0; i <= n; i++) {
+//                for (int j = 0; j <= Math.min(i, k); j++) {
+//                    System.out.printf("%5d ", dp[i][j]);
+//                }
+//                System.out.println();
+//            }
+//
+//            System.out.println(dp[n][k]);
+            bw.write(String.valueOf(dp[n][k]));
+            bw.write("\n");
+
+        }
+        br.close();
+        bw.close();
+    }
 }
